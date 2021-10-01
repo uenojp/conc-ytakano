29ba2329116996715b7b2f594c3c8752ff1f38b9
Ueno <uenotakahiro.jp@gmail.com>
2021-10-01 17:05:06 +0900

    posix-semaphore

--- chap3/posix-semaphore.c
+++ chap3/posix-semaphore.c
@@ -3,7 +3,8 @@
 
    そのために、CS内でカウンタを
     - インクリメント
-    - スリープ (複数作ったプロセスを並行状態にするため、インク、デクリメントだけでは速すぎて並行状態にならない)
+    - スリープ
+   (複数作ったプロセスを並行状態にするため、インク、デクリメントだけでは速すぎて並行状態にならない)
     - デクリメント
    し、カウントの値が常にN以下であることを確認する
    仮に、カウントがN以上になると、インクリメントはCS内の操作であるため
@@ -19,25 +20,56 @@
 
 const int NUM_THREADS = 10;
 const int NUM_LOOP = 10;  // スレッド内のループ数
-const int N;              // CSへ入れるプロセスの数
+const int N = 3;          // CSへ入れるプロセスの数
 
+/* CSに入ったスレッド数(セマフォを扱う際に必要な変数ではない)
+   ただCSにいくつスレッドが入ったかを確認したい */
 int count = 0;
 
 void* th(void* arg) {
+    sem_t* sem = sem_open("/mysemaphore", 0);
+    if (sem == SEM_FAILED) {
+        perror("sem_open in th");
+        exit(1);
+    }
     for (int i = 0; i < NUM_LOOP; i++) {
-        /* クリティカルセクション*/
-        count += 1;
+        if (sem_wait(sem) == -1) {
+            perror("sem_wait");
+            exit(1);
+        }
+
+        /* クリティカルセクション */
+
+        /* カウンタをatomiにインクリメント */
+        __sync_fetch_and_add(&count, 1);
+        printf("%d\n", count);
+
         usleep(10000);
-        count += 1;
+
+        __sync_fetch_and_sub(&count, 1);
+
         /* ここまで */
+
+        /* セマフォの値を増やす、つまり、CSに入れるスレッドの数を増やす */
+        if (sem_post(sem) == -1) {
+            perror("sem_post");
+            exit(1);
+        }
+    }
+    if (sem_close(sem) == -1) {
+        perror("sem_close");
+        exit(1);
     }
     return NULL;
 }
 
 int main(int argc, char** argv) {
+    /* permission deniedで弾かれる
+       sudoで実行してもだめだった
+       ubuntuのコンテナ立ててそこでやったら(root)できた、なぞ */
     sem_t* s = sem_open("/mysemaphore", O_CREAT, 0066, N);
     if (s == SEM_FAILED) {
-        perror("sem_open");
+        perror("sem_open in main");
         return 1;
     }
 
